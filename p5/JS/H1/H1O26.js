7d826893230091ef9ff804a362b31fb8e517dd64
Miguel Bastos <145102949+MiguelMaestro1@users.noreply.github.com>
2023-11-24 10:21:17 +0100

    werk

--- p5/JS/H1/H1O26.js
+++ p5/JS/H1/H1O26.js
@@ -11,7 +11,7 @@ function draw() {
   rect(0,250,width,height - 250);
   translate(125,0);
 
-  for (var n = 1;n <= 4;n++) {
+  for (var n = 0;n <= 10;n++) {
     tekenHuis();
     tekenBoom(n);
     translate(200,0);
--- p5/JS/H1/H1O28.js
+++ p5/JS/H1/H1O28.js
@@ -1,4 +1,4 @@
-var aantal = 5;
+var aantal = 15;
 
 function setup() {
   canvas = createCanvas(450,450);
@@ -18,15 +18,21 @@ function draw() {
   
   // tekenen rode bladeren
   
-  fill(178, 34, 34,.7);
+  fill(255, 0, 34,.7);
+for (var i = 0; i < aantal; i++) {
   ellipse(0,0,400,50);
   rotate(360 / aantal);
+}
 
-  
+
+
   // gele vulkleur
 
-  fill(255, 195, 0,0.5);
-
+  fill(150, 199, 0,0.5);
+for(var i = 0; i < aantal; i++) {
+  rect(0,0,75,75)
+  rotate(360/aantal)
+}
 
   if (keyIsDown(LEFT_ARROW) && aantal >= 2) {
     aantal--;
