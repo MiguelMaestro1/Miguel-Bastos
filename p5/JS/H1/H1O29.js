cde3b2dba4ee49fba0e568391fde63ee60d66161
Miguel Bastos <145102949+MiguelMaestro1@users.noreply.github.com>
2023-12-01 10:05:04 +0100

    up

--- p5/JS/H1/H1O29.js
+++ p5/JS/H1/H1O29.js
@@ -1,5 +1,5 @@
 var aantal = 5;
-
+var scale;
 function setup() {
   canvas = createCanvas(450,450);
   canvas.parent('processing');
@@ -14,9 +14,9 @@ function draw() {
   background('lavender');
   fill('black');
   text("aantal = " + aantal,10,20);    
-  translate(225,225);
+  translate(100,100);
   fill(178, 34, 34,0.7);
-  for (var n = 0;n < aantal;n++) {
+  for (var x = 0;x < 6;x++) {
     ellipse(0,0,400,50);
     rotate(360 / aantal);
   }
@@ -33,3 +33,9 @@ function draw() {
   }   
 }
 
+function tekenBloem() {
+  background('orange');
+  fill('wheat');
+  rect(0,250,width,height - 250);
+  translate(125,0);
+}
--- p5/JS/H1/H1O30.js
+++ p5/JS/H1/H1O30.js
@@ -4,11 +4,12 @@ function setup() {
   canvas = createCanvas(451,451);
   canvas.parent('processing');
   background('coral');
-  fill('white');
 }
 
 function draw() {
+  for (var rij = 0; rij < 450;rij += 50)
     for (var kolom = 0;kolom < 450;kolom += 50) {
+      fill(kleur)
       rect(kolom,rij,50,50);
     }
 }
