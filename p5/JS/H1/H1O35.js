49dc94c86e43239647aaf5aa8f17f87c780b61a3
Miguel Bastos <145102949+MiguelMaestro1@users.noreply.github.com>
2023-12-15 09:41:49 +0100

    Update H1O35.js

--- p5/JS/H1/H1O35.js
+++ p5/JS/H1/H1O35.js
@@ -13,18 +13,17 @@ function setup() {
 }
 
 function draw() {
-  for (var laag=1;laag<=aantalLagen;laag++){
-   tekenRij(laag);
-   translate(0,hoogte);
-   }
+  translate(0,height-hoogte);
+  tekenPiramide(aantalLagen);
+ }
+ function tekenPiramide(n) {
+  if (n>0) {
+  for (var nr = 0;nr < n;nr++) {
+  rect(nr*breedte,0,breedte,hoogte);
   }
-  function tekenRij(aantalStenen) {
-   inspringen =
-   (aantalLagen-aantalStenen)*0.5*breedte;
-   push();
-   translate(inspringen,0);
-   for (var steen = 0;steen < 4;steen++) {
-   rect(breedte*steen,0,breedte,hoogte);
-   }
-   pop();
+  translate(breedte/2,-hoogte);
+  n--;
+  tekenPiramide(n);
   }
+ }
+ 
